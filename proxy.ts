5c9e52b93925d0f0eeda08db7d230c0b708905fe
Rondell Cobbs II <ipheroics@gmail.com>
2026-01-26 15:11:31 -0500

    refactor: migrate middleware to proxy to fix deprecation warning

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 
-export async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
